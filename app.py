06f0ebebb35fa025cdd5b518233e60872b508f58
Lahiru Jayakodi <75355480+codeoxygen@users.noreply.github.com>
2023-06-15 16:27:31 +0530

    update the body

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from langchain.memory import ConversationBufferMemory
 
 from chatTemplate import css , bot_template , user_template
 
-def get_pdf_text(pdf_docs):
+def get_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
         pdf_reader = PdfReader(pdf)
@@ -23,7 +23,7 @@ def pattern_matching(user_input):
     replaced_input = re.sub(pattern , "document" , user_input,flags=re.IGNORECASE)
     return replaced_input
 
-def get_text_chunks(text):
+def get_chunks(text):
     text_splitter = CharacterTextSplitter(
         separator="\n",
         chunk_size=1000,
@@ -36,13 +36,46 @@ def get_text_chunks(text):
 
 def get_vectorstore(text_chunks):
     embeddings = OpenAIEmbeddings()
-    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
-def get_conversation_chain(vectorstore):
+
+ 
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    chat_html = ""
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            chat_html += user_template.replace("{{MSG}}", message.content) + "\n"
+        else:
+            chat_html += bot_template.replace("{{MSG}}", message.content) + "\n"
+
+    # Update chat section with newest message
+    st.markdown(chat_html, unsafe_allow_html=True)
+
+    # Scroll to the bottom
+    scroll_down_script = """
+    <script>
+    var chatElement = document.getElementsByClassName("chat-container")[0];
+    chatElement.scrollTop = chatElement.scrollHeight;
+    </script>
+    """
+    st.markdown(scroll_down_script, unsafe_allow_html=True)
+    
+    # Scroll to the top
+    scroll_up_script = """
+    <script>
+    var chatElement = document.getElementsByClassName("chat-container")[0];
+    chatElement.scrollTop = 0;
+    </script>
+    """
+    st.markdown(scroll_up_script, unsafe_allow_html=True)
+
+def get_conv_chain(vectorstore):
     llm = ChatOpenAI()
-    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+  
 
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
@@ -53,23 +86,11 @@ def get_conversation_chain(vectorstore):
     )
     return conversation_chain
 
- 
-def handle_userinput(user_question):
-    response = st.session_state.conversation({'question': user_question})
-    st.session_state.chat_history = response['chat_history']
 
-    for i, message in enumerate(st.session_state.chat_history):
-        if i % 2 == 0:
-            st.write(user_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
-        else:
-            st.write(bot_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
 
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Chat with multiple PDFs",
-                       page_icon=":books:")
+    st.set_page_config(page_title="DocuChat", page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
@@ -77,32 +98,32 @@ def main():
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
 
-    st.header("Chat with multiple PDFs :books:")
-    user_question = st.text_input("Ask a question about your documents:")
-    if user_question:
-        
-        handle_userinput(user_question)
+    st.title("DocuChat :books:")
+
+    col1, col2 = st.columns([4, 1])
+    user_question = col1.text_input("Ask a question about your documents:")
+
+    if col2.button("Ask"):
+        with st.spinner("Processing your question..."):
+            if user_question:
+                handle_userinput(user_question)
+
+    st.markdown("---")
 
     with st.sidebar:
         st.subheader("Your documents")
         pdf_docs = st.file_uploader(
             "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        
+        
+        
+        
         if st.button("Process"):
             with st.spinner("Processing"):
-                # get pdf text
-                raw_text = get_pdf_text(pdf_docs)
-
-                # get the text chunks
-                text_chunks = get_text_chunks(raw_text)
-
-                # create vector store
+                raw_text = get_text(pdf_docs)
+                text_chunks = get_chunks(raw_text)
                 vectorstore = get_vectorstore(text_chunks)
+                st.session_state.conversation = get_conv_chain(vectorstore)
 
-                # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
-
-if __name__ =="__main__":
+if __name__ == "__main__":
     main()
-
-          
